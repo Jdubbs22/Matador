92438fd34214a57ce62310d4ed4e9ee79c37ca83
John <cpd015@gmail.com>
2018-09-23 18:20:05 -0500

    first commit

---
+++ src/main/java/edu/depaul/cdm/se/matador/MatadorApplication.java
@@ -0,0 +1,12 @@
+package edu.depaul.cdm.se.matador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatadorApplication.class, args);
+	}
+}
